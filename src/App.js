aa838fc5bf44ba2cb1f7f3230b37407bb36bad40
claraamz <clara.amazonas08@gmail>
2026-03-19 21:15:10 -0300

    app.js modificado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+const Equipe = (props) => {
+    return (
+        <div>
+            <Sobre usuario={props.nome} 
+            anos={props.idade} 
+            proficao={props.cargo}/>
+            <Social link={props.link} 
+            github={props.github}/>
+        </div>
+    )
+}
+ const Sobre = (props) => {
+    return(
+        <div>
+            <h2> OLá, sou a {props.usuario}</h2>
+            <p> Tenho {props.anos} anos e trabalho como {props.proficao}</p>
+        </div>
+    )
+ }
+ const Social = () => {
+    return(
+        <div>
+            <h3> Redes sociais</h3>
+            <ul>
+                <li><a href={"https://www.linkedin.com/in/clara"}>LinkedIn</a></li>
+                <li><a href="https://www.github.com/clara">GitHub</a></li>
+            </ul>
+        </div>
+    )
+ }
 export default function App() {
-  return (
-    <div>
-      <h1>Hello, React app!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1> Conheça a nossa equipe</h1>
+            <Equipe nome="Clara" cargo="Desenvolvedora" idade="19" linkedin="https://www.linkedin.com/in/clara" github="https://www.github.com/clara"/>
+            <Equipe nome="Geovanna" cargo="Desenvolvedora" idade="19" linkedin="https://www.linkedin.com/in/geovanna" github="https://www.github.com/geovanna"/>
+            <Equipe nome="Delrick" cargo="Desenvolvedor" idade="27" linkedin="https://www.linkedin.com/in/delrick" github="https://www.github.com/delrick"/>
+        </div>
+    )
 }
